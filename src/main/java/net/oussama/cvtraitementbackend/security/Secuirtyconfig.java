7bd1b9e64dc6cf4b8754e6a0b997d17a9e6dca7f
hamdane1548 <oussamahamdane80@gmail.com>
2026-01-10 14:00:47 +0100

    intial secuirty config

--- src/main/java/net/oussama/cvtraitementbackend/security/Secuirtyconfig.java
+++ src/main/java/net/oussama/cvtraitementbackend/security/Secuirtyconfig.java
@@ -4,11 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class Secuirtyconfig {
-
+    @Bean InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin").password(encoder.encode("12345")).roles("ADMIN").build(),
+                User.withUsername("user").password(encoder.encode("12345")).roles("USER").build()
+        );
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
